6ec9cb1d148e9795f86dc7e9e33a4da4903e5d43
Alexandre Genon <alexandre.genon@gmail.com>
2015-10-16 09:30:42 +0200

    clean mistake

--- .worksheet/src/test.scala
+++
@@ -1,7 +0,0 @@
-object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(57); 
-  println("Welcome to the Scala worksheet");$skip(41); 
-  
-  val solver=core.Solver(List(1,2),3);System.out.println("""solver  : core.Solver.Solver = """ + $show(solver ));$skip(37); 
-  val paths = solver.pathSets.toList;System.out.println("""paths  : List[Set[core.Solver.Path]] = """ + $show(paths ));$skip(44); val res$0 = 
-  for (path <- paths) yield {println(path)};System.out.println("""res0: List[Unit] = """ + $show(res$0))}
-}
